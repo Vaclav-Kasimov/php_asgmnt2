78ff65d0d10ac3d9960325bcc263ff6357d473b9
Vaclav-Kasimov <kasimov.vyacheslav.aaa@gmail.com>
2023-02-04 16:46:25 +0300

    index.php modified, addedbasic HTML-code

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Kasimov Viacheslav PHP</title>
+    </head>
+    <body>
+        <h1>Kasimov Viacheslav PHP</h1>
+        <pre></pre>
+    </body>
+</html>
