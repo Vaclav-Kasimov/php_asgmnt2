a7ea56eab629edd602d67c8f297345f214ba5702
Vaclav-Kasimov <kasimov.vyacheslav.aaa@gmail.com>
2023-02-08 19:57:47 +0300

    done

--- index.php
+++ index.php
@@ -5,6 +5,27 @@
     </head>
     <body>
         <h1>Kasimov Viacheslav PHP</h1>
-        <pre></pre>
+        <pre>
+    VVVVVVVV           VVVVVVVV
+    V::::::V           V::::::V
+    V::::::V           V::::::V
+    V::::::V           V::::::V
+     V:::::V           V:::::V 
+      V:::::V         V:::::V  
+       V:::::V       V:::::V   
+        V:::::V     V:::::V    
+         V:::::V   V:::::V     
+          V:::::V V:::::V      
+           V:::::V:::::V       
+            V:::::::::V        
+             V:::::::V         
+              V:::::V          
+               V:::V           
+                VVV            
+                 V                  
+        </pre>
+        <div>
+            sha256 for Kasimov Viacheslav is: <?php echo hash('sha256', 'Kasimov Viacheslav'); ?>
+        </div>
     </body>
 </html>
